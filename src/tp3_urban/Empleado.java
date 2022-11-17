c57004e27a85a7892568cbf6bde430cef207d63c
nejito <danielurban473@gmail.com>
2022-11-17 14:54:10 -0300

    primer

---
+++ src/tp3_urban/Empleado.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3_urban;
+
+/**
+ *
+ * @author nejito
+ */
+public class Empleado {
+    private String nombre;
+    private int salario;
+    private int porcentaje;
+}
---
+++ src/tp3_urban/Personas.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3_urban;
+
+/**
+ *
+ * @author nejo
+ */
+public class Personas {
+    private String nombre;
+    private String sexo;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+    
+    
+}
